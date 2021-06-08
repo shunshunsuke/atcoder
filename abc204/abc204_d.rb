f8d84169f5f5b76ffe59688bb51cf01f0e977c98
shunshunsuke <shunsuke.oda@rakuten.com>
2021-06-08 11:38:40 +0900

    提出

--- abc204/abc204_d.rb
+++ abc204/abc204_d.rb
@@ -1,23 +1,20 @@
 N = gets.to_i
 T = gets.split.map(&:to_i)
-if N == 1
-  puts T[0]
-  exit
-end
-T.sort!
-o1 = T.pop
-o2 = T.pop
-t1 = o1
-t2 = o2
-until T.empty?
-  if o1 > o2
-    o1 -= o2
-    o2 = T.pop
-    t2 += o2
-  else
-    o2 -= o1
-    o1 = T.pop
-    t1 += o1
+W = T.sum / 2
+dp = Array.new(N + 1) { Array.new(W + 1, false) }
+dp[0][0] = true
+N.times do |i|
+  (W + 1).times do |w|
+    next unless dp[i][w]
+    # i番目の料理を選ぶ場合
+    dp[i + 1][w + T[i]] = true if w + T[i] <= W
+    # i番目の料理を選ばない場合
+    dp[i + 1][w] = true
   end
 end
-puts t1 > t2 ? t1 : t2
+dp[N].each_with_index.reverse_each do |flag, w|
+  if flag
+    puts T.sum - w
+    break
+  end
+end
