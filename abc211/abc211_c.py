6c10f3b90f2a674920942e91966e70d18b8e92c7
shunshunsuke <shunsuke.oda@rakuten.com>
2021-07-24 23:46:28 +0900

    参加

---
+++ abc211/abc211_c.py
@@ -0,0 +1,20 @@
+S = input()
+dp = [0] * 9
+for s in S:
+    if s == 'c':
+        dp[1] += 1
+    elif s == 'h':
+        dp[2] += dp[1]
+    elif s == 'o':
+        dp[3] += dp[2]
+    elif s == 'k':
+        dp[4] += dp[3]
+    elif s == 'u':
+        dp[5] += dp[4]
+    elif s == 'd':
+        dp[6] += dp[5]
+    elif s == 'a':
+        dp[7] += dp[6]
+    elif s == 'i':
+        dp[8] += dp[7]
+print(dp[8] % (10**9 + 7))
