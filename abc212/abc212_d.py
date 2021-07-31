cb95c24222bfd39db47d7a9b0706189b7343206a
shunshunsuke <shunsuke.oda@rakuten.com>
2021-08-01 00:05:49 +0900

    提出

--- abc212/abc212_d.py
+++ abc212/abc212_d.py
@@ -1,17 +1,14 @@
+import heapq
+
 Q = int(input())
 ll = []
-prev = 0
-pn = 0
+heapq.heapify(ll)
+sk = 0
 for _ in range(Q):
     px = list(map(int, input().split()))
     if px[0] == 1:
-        ll.append(px[1])
+        heapq.heappush(ll, px[1] - sk)
     elif px[0] == 2:
-        pn += px[1]
+        sk += px[1]
     else:
-        if prev != 3:
-            ll.sort(reverse=True)
-            ll = list(map(lambda x: x + pn, ll))
-            pn = 0
-        print(ll.pop(-1))
-    prev = px[0]
+        print(heapq.heappop(ll) + sk)
