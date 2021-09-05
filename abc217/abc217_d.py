068bad14cef7a45be33322e9238bf963d8e14cf5
shunshunsuke <shunsuke.oda@rakuten.com>
2021-09-05 10:51:38 +0900

    listからarrayに変更することでTLEを回避

--- abc217/abc217_d.py
+++ abc217/abc217_d.py
@@ -1,19 +1,13 @@
 from sys import stdin
+import array
 import bisect
 
 L, Q = [int(x) for x in stdin.readline().rstrip().split()]
-separation = []
+separation = array.array('i', [0, L])
 for _ in range(Q):
     c, x = [int(x) for x in stdin.readline().rstrip().split()]
     if c == 1:
         bisect.insort(separation, x)
     else:
         i = bisect.bisect(separation, x)
-        if len(separation) == 0:
-            print(L)
-        elif i == 0:
-            print(separation[0])
-        elif i == len(separation):
-            print(L - separation[-1])
-        else:
-            print(separation[i] - separation[i - 1])
+        print(separation[i] - separation[i - 1])
