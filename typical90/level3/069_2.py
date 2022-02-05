d361a479e739f80739653828198e77dcf3880fb8
shunshunsuke <shunsuke.oda@rakuten.com>
2022-02-05 17:23:24 +0900

    提出

--- typical90/level3/069_2.py
+++ typical90/level3/069_2.py
@@ -1,14 +1,5 @@
 from sys import stdin
 
-def pow(x, n):
-    ans = 1
-    while n > 0:
-        if n % 2:
-            ans = ans * x % mod
-        x = x * x % mod
-        n //= 2
-    return ans
-
 N, K = [int(x) for x in stdin.readline().rstrip().split()]
 mod = 10**9 + 7
 if N == 1:
@@ -19,4 +10,4 @@ else:
     if K <= 2:
         print(0)
     else:
-        print(K * (K - 1) % mod * pow(K - 2, N - 2) % mod)
+        print(K * (K - 1) % mod * pow(K - 2, N - 2, mod) % mod)
